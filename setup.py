f50f48a97768eaea387d98ec7ccb3dfdaa21c910
Zuzanna Opała <opala.zuzanna@gmail.com>
2019-11-18 13:33:50 +0100

    Add basic pip repository structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gfootball_zpp',
+      version='0.1',
+      description='The repository for multiagent gfootball ZPP project 2019/2020.',
+      url='http://github.com/wdomitrz/gfootball_zpp',
+      author='(alphabetically) Witalis Domitrz, Zuzanna Opała, Mateusz Sieniawski, Kondrad Staniszewski',
+      author_email='',
+      license='???',
+      packages=['gfootball_zpp'],
+      zip_safe=False)
