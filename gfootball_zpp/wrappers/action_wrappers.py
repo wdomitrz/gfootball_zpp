f31442c5b51df1885b64bb54a5d9a25428074f4b
Zuzanna Opała <opala.zuzanna@gmail.com>
2019-11-20 16:36:15 +0100

    Update files to it's current version from SEED repo

--- gfootball_zpp/wrappers/action_wrappers.py
+++ gfootball_zpp/wrappers/action_wrappers.py
@@ -7,7 +7,7 @@ def encoded_player_position(observation):
   player_position = observation[team][player_id]
   return np.floor(player_position[0] * 1000.0) + player_position[1]
 
-class ActionOrder(gym.Wrapper):  
+class ActionOrder(gym.Wrapper):
   def __init__(self, env, env_config, piority_fn=encoded_player_position):
     gym.Wrapper.__init__(self, env)
     self._last_observation = None
@@ -20,7 +20,7 @@ class ActionOrder(gym.Wrapper):
     permuted_action[permutation] = action[...]
     # print('got:', action, permutation, 'returns:', permuted_action)
     return permuted_action
-  
+
   def step(self, action):
     observation, reward, done, info = self.env.step(self.permute(action))
     self._last_observation = observation
