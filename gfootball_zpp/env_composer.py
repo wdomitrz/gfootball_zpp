51f6d5bf779a99002069811c15c0a219926a6ea3
Zuzanna Opała <opala.zuzanna@gmail.com>
2019-11-20 16:36:15 +0100

    Update files to it's current version from SEED repo

--- gfootball_zpp/env_composer.py
+++ gfootball_zpp/env_composer.py
@@ -1,27 +1,55 @@
 from gfootball.env import config
 from gfootball.env import football_env
-from gfootball.env import observation_preprocessing
 from gfootball.env import wrappers
 
-from action_wrappers import ActionOrder
-from player_stack_wrapper import PlayerStackWrapper
+from .wrappers.action_wrappers import ActionOrder
+from .wrappers.player_stack_wrapper import PlayerStackWrapper
+import collections
+import gym
+import numpy as np
 
-def dump_wrapper(env, env_config):
-  if env_config['dump_frequency'] > 1:
-    return wrappers.PeriodicDumpWriter(env, env_config['dump_frequency'])
+class FrameStack(gym.Wrapper):
+  """Stack k last observations."""
+
+  def __init__(self, env, k):
+    gym.Wrapper.__init__(self, env)
+    self.obs = collections.deque([], maxlen=k)
+    low = env.observation_space.low
+    high = env.observation_space.high
+    low = np.concatenate([low] * k, axis=-1)
+    high = np.concatenate([high] * k, axis=-1)
+    self.observation_space = gym.spaces.Box(
+        low=low, high=high, dtype=env.observation_space.dtype)
+
+  def reset(self):
+    observation = self.env.reset()
+    self.obs.extend([observation] * self.obs.maxlen)
+    return self._get_observation()
+
+  def step(self, action):
+    observation, reward, done, info = self.env.step(action)
+    self.obs.append(observation)
+    return self._get_observation(), reward, done, info
+
+  def _get_observation(self):
+    return np.concatenate(list(self.obs), axis=-1)
+
+def dump_wrapper(env, config):
+  if config['dump_frequency'] > 1:
+    return wrappers.PeriodicDumpWriter(env, config['dump_frequency'])
   else:
     return env
 
-def checkpoint_wrapper(env, env_config):
-  assert 'scoring' in env_config['rewards'].split(',')
-  if 'checkpoints' in env_config['rewards'].split(','):
+def checkpoint_wrapper(env, config):
+  assert 'scoring' in config['rewards'].split(',')
+  if 'checkpoints' in config['rewards'].split(','):
     return wrappers.CheckpointRewardWrapper(env)
   else:
     return env
-    
-def single_agent_wrapper(env, env_config):
-  if (env_config['number_of_left_players_agent_controls'] +
-      env_config['number_of_right_players_agent_controls'] == 1):
+
+def single_agent_wrapper(env, config):
+  if (config['number_of_left_players_agent_controls'] +
+      config['number_of_right_players_agent_controls'] == 1):
      env = wrappers.SingleAgentObservationWrapper(env)
      env = wrappers.SingleAgentRewardWrapper(env)
      return env
@@ -32,35 +60,12 @@ KNOWN_WRAPPERS = {
   'periodic_dump': dump_wrapper,
   'checkpoint_score': checkpoint_wrapper,
   'single_agent': single_agent_wrapper,
-  'obs_extract': lambda env, env_config: wrappers.SMMWrapper(env, env_config['channel_dimensions']),
-  'obs_stack': lambda env, env_config: wrappers.FrameStack(env, env_config['stacked_frames']),
-  'action_order': ActionOrder
-}
-  
-DEFAULT_BASIC_CONFIG = {
-  'env_name': 'academy_3_vs_1_with_keeper',                
-  'enable_goal_videos': False,
-  'enable_full_episode_videos': False,
-  'logdir': '',
-  'enable_sides_swap': False,
-  'number_of_left_players_agent_controls': 2,
-  'number_of_right_players_agent_controls': 0,
-  'extra_players': None,
-  'render': False,
-  'write_video': False
+  'obs_extract': lambda env, config: wrappers.SMMWrapper(env, config['channel_dimensions']),
+  'obs_stack': lambda env, config: FrameStack(env, config['stacked_frames']),
+  'action_order': ActionOrder,
+  'psw': PlayerStackWrapper,
 }
 
-DEFAULT_EXTENDED_CONFIG = {
-  'stacked_frames': 4,
-  'channel_dimensions': (observation_preprocessing.SMM_WIDTH,
-                         observation_preprocessing.SMM_HEIGHT),
-  'rewards': 'scoring',
-  'dump_frequency': 1,
-  'wrappers': 'periodic_dump,checkpoint_score,single_agent,obs_extract,obs_stack'
-}
-
-DEFAULT_EXTENDED_CONFIG = {**DEFAULT_BASIC_CONFIG, **DEFAULT_EXTENDED_CONFIG}
-  
 def compose_environment(env_config, wrappers):
   def extract_from_dict(dictionary, keys):
     return {new_k: dictionary[k] for (new_k, k) in keys}
@@ -87,22 +92,20 @@ def compose_environment(env_config, wrappers):
 
   return env
 
-def cmd_compose_environment(flags):
-  """todo"""
-  env_config = {**DEFAULT_EXTENDED_CONFIG, **flags}
+def config_compose_environment(config):
   wrappers = []
-  for w in env_config['wrappers'].split(','):
+  for w in config['wrappers'].split(','):
     wrappers.append(KNOWN_WRAPPERS[w])
 
-  return compose_environment(env_config, wrappers)
-  
-def sample_composed_environment():
-  return compose_environment(DEFAULT_EXTENDED_CONFIG, [
-    dump_wrapper,
-    checkpoint_wrapper,
-    ActionOrder,
-    KNOWN_WRAPPERS['obs_extract'],
-    single_agent_wrapper,
-    KNOWN_WRAPPERS['obs_stack'],
-    PlayerStackWrapper],
-  )
+  return compose_environment(config, wrappers)
+
+# def sample_composed_environment():
+#   return compose_environment(DEFAULT_EXTENDED_CONFIG, [
+#     dump_wrapper,
+#     checkpoint_wrapper,
+#     ActionOrder,
+#     lambda env, config: wrappers.SMMWrapper(env, config['channel_dimensions']),
+#     single_agent_wrapper,
+#     lambda env, config: FrameStack(env, config['stacked_frames']),
+#     PlayerStackWrapper],
+#   )
