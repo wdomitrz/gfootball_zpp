1a2638d6c9492fa91182544bcc32b342684ecc66
Zuzanna Opała <opala.zuzanna@gmail.com>
2020-02-10 14:55:08 +0100

    Add function to play on leaderboard

--- leaderboard/example/example.py
+++ leaderboard/example/example.py
@@ -1,16 +1,26 @@
 import tensorflow as tf
 from absl import flags
 from absl import app
-from gfootball.env import create_environment
+from absl import logging
+from gfootball.env import create_environment, create_remote_environment
+import grpc
 
 from network import GFootball
 from old_1_multihead_net import MultiHeadNet
 import observation
 
 
-
 flags.DEFINE_string('ckpt', './f5v5_bots_one_net_then_self_play_3_2_1_ckpt_0_ckpt-456', 'Path to checkpoint')
+flags.DEFINE_string('username', 'multiPandasUW', 'Username to use')
+flags.DEFINE_string('token', None, 'Token to use.')
+flags.mark_flag_as_required('token')
+flags.DEFINE_string('track', 'multiagent', 'Name of the competition track.')
+flags.DEFINE_string('model_name', None,
+                    'A model identifier to be displayed on the leaderboard.')
+flags.mark_flag_as_required('model_name')
+flags.DEFINE_integer('how_many', 1000, 'How many games to play')
 
+flags.DEFINE_bool('render', False, 'Whether to render a game.')
 FLAGS = flags.FLAGS
 
 
@@ -24,32 +34,40 @@ def convert_observations(obs):
     return (), ((), (), obs)
 
 
-def example_play_with_bots():
-    env = create_environment('5_vs_5', stacked=True, representation='extracted', rewards='scoring',
-                             render=True, number_of_left_players_agent_controls=4)
-    # Wrapper, second argument is unused
+def wrap_env(env):
     env = MultiHeadNet(env, ())
 
     # Used in order to maintain compatibility with network
     # Packing makes number of minimap layers divisible by 16
     env = observation.PackedBitsObservation(env)
+    return env
 
-    obs = env.reset()
-    print(obs.shape)
-    
+
+def restore_checkpoint(env, sample_obs):
     net = GFootball(env.action_space.nvec)
     net.change_config({'sample_actions': False})
-    # create variables
-    print(net(convert_observations(obs), ()))
 
-    #load checkpoint
-    optimizer = tf.keras.optimizers.Adam(0.01) # only to supress some warnings
+    # create variables
+    net(convert_observations(sample_obs), ())
+
+    # load checkpoint
+    optimizer = tf.keras.optimizers.Adam(0.01)  # only to supress some warnings
     checkpoint = tf.train.Checkpoint(optimizer=optimizer, agent=net)
     print('restoring:', FLAGS.ckpt)
     status = checkpoint.restore(FLAGS.ckpt)
     print(status)
+    return net
 
 
+def example_play_with_bots():
+    env = create_environment('5_vs_5', stacked=True, representation='extracted', rewards='scoring',
+                             render=True, number_of_left_players_agent_controls=4)
+    # Wrapper, second argument is unused
+    env = wrap_env(env)
+
+    net = restore_checkpoint(env)
+
+    obs = env.reset()
     done = False
     while not done:
         obs = convert_observations(obs)
@@ -59,5 +77,57 @@ def example_play_with_bots():
         obs, rew, done, _ = env.step(action)
 
 
+def leaderboard(unused_argv):
+  env = create_remote_environment(
+      FLAGS.username, FLAGS.token, FLAGS.model_name, track=FLAGS.track,
+      representation='raw', stacked=True, include_rendering=FLAGS.render)
+  env = wrap_env(env)
+  obs = env.reset()
+  model = restore_checkpoint(env, obs)
+  for _ in range(FLAGS.how_many):
+      ob = obs
+      cnt = 1
+      done = False
+      while not done:
+          try:
+              ob = convert_observations(ob)
+              agent_output, _ = model(ob, ())
+              action = agent_output.action.numpy().flatten()
+              ob, rew, done, _ = env.step(action)
+              logging.info('Playing the game, step %d, action %s, rew %s, done %d',
+                           cnt, action, rew, done)
+              cnt += 1
+          except grpc.RpcError as e:
+              print(e)
+              break
+      print('=' * 50)
+
+def leaderboard_test(unused_argv):
+  env = create_environment('5_vs_5', stacked=True, representation='extracted',
+                           rewards='scoring', render=True,
+                           number_of_left_players_agent_controls=4)
+  env = wrap_env(env)
+  obs = env.reset()
+  model = restore_checkpoint(env, obs)
+  for _ in [0]:#range(FLAGS.how_many):
+    ob = obs
+    cnt = 1
+    done = False
+    while not done:
+      try:
+        ob = convert_observations(ob)
+        agent_output, _ = model(ob, ())
+        action = agent_output.action.numpy().flatten()
+        ob, rew, done, _ = env.step(action)
+        logging.info('Playing the game, step %d, action %s, rew %s, done %d',
+                     cnt, action, rew, done)
+        cnt += 1
+      except grpc.RpcError as e:
+        print(e)
+        break
+    print('=' * 50)
+
+
+
 if __name__ == '__main__':
-    app.run(lambda _: example_play_with_bots())
+    app.run(leaderboard)
