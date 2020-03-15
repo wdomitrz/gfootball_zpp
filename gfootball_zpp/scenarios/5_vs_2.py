8b44185dd5f523f16d71b51e10c840675592a0db
wdomitrz <witekdomitrz@gmail.com>
2020-03-15 15:32:27 +0100

    5 vs 2,3

--- gfootball_zpp/scenarios/5_vs_2.py
+++ gfootball_zpp/scenarios/5_vs_2.py
@@ -15,7 +15,6 @@
 
 from gfootball.scenarios import *
 
-
 def build_scenario(builder):
   builder.config().game_duration = 3000
   builder.config().right_team_difficulty = 0.05
@@ -24,18 +23,24 @@ def build_scenario(builder):
   if builder.EpisodeNumber() % 2 == 0:
     first_team = Team.e_Left
     second_team = Team.e_Right
+    builder.SetTeam(first_team)
+    builder.AddPlayer(-1.000000, 0.000000, e_PlayerRole_GK, controllable=False)
+    builder.AddPlayer(0.000000,  0.020000, e_PlayerRole_RM)
+    builder.AddPlayer(0.000000, -0.020000, e_PlayerRole_CF)
+    builder.AddPlayer(-0.1, -0.1, e_PlayerRole_LB)
+    builder.AddPlayer(-0.1,  0.1, e_PlayerRole_CB)
+    builder.SetTeam(second_team)
+    builder.AddPlayer(-1.000000, 0.000000, e_PlayerRole_GK, controllable=False)
+    builder.AddPlayer(-0.04, -0.0, e_PlayerRole_CF)
   else:
     first_team = Team.e_Right
     second_team = Team.e_Left
-  builder.SetTeam(first_team)
-  builder.AddPlayer(-1.000000, 0.000000, e_PlayerRole_GK, controllable=False)
-  builder.AddPlayer(0.000000,  0.020000, e_PlayerRole_RM)
-  builder.AddPlayer(0.000000, -0.020000, e_PlayerRole_CF)
-  builder.AddPlayer(-0.1, -0.1, e_PlayerRole_LB)
-  builder.AddPlayer(-0.1,  0.1, e_PlayerRole_CB)
-  builder.SetTeam(second_team)
-  builder.AddPlayer(-1.000000, 0.000000, e_PlayerRole_GK, controllable=False)
-  builder.AddPlayer(-0.04,  0.040000, e_PlayerRole_RM)
-  builder.AddPlayer(-0.04, -0.040000, e_PlayerRole_CF)
-  builder.AddPlayer(-0.1, -0.1, e_PlayerRole_LB)
-  builder.AddPlayer(-0.1,  0.1, e_PlayerRole_CB)
+    builder.SetTeam(first_team)
+    builder.AddPlayer(-1.000000, 0.000000, e_PlayerRole_GK, controllable=False)
+    builder.AddPlayer(0.0, 0.0, e_PlayerRole_CF)
+    builder.SetTeam(second_team)
+    builder.AddPlayer(-1.000000, 0.000000, e_PlayerRole_GK, controllable=False)
+    builder.AddPlayer(-0.04,  0.040000, e_PlayerRole_RM)
+    builder.AddPlayer(-0.04, -0.040000, e_PlayerRole_CF)
+    builder.AddPlayer(-0.1, -0.1, e_PlayerRole_LB)
+    builder.AddPlayer(-0.1,  0.1, e_PlayerRole_CB)
