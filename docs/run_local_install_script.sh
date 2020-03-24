73a230054aa234fdc96a007c5cecdc6346432422
CStanKonrad <CStanKonrad@users.noreply.github.com>
2020-03-24 20:40:40 +0100

    Blog run local tutorial

---
+++ docs/run_local_install_script.sh
@@ -0,0 +1,57 @@
+sudo apt-get update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo usermod -aG docker $USER;
+
+# Following part adopted from https://www.tensorflow.org/install/gpu#ubuntu_1804_cuda_101
+# Add NVIDIA package repositories
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo dpkg -i cuda-repo-ubuntu1804_10.1.243-1_amd64.deb
+sudo apt-get update
+wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+sudo apt-get update
+
+# Install NVIDIA driver
+sudo apt-get install --no-install-recommends nvidia-driver-430
+# Reboot. Check that GPUs are visible using the command: nvidia-smi
+
+# Install development and runtime libraries (~4GB)
+sudo apt-get install --no-install-recommends \
+    cuda-10-1 \
+    libcudnn7=7.6.4.38-1+cuda10.1  \
+    libcudnn7-dev=7.6.4.38-1+cuda10.1
+
+
+# Install TensorRT. Requires that libcudnn7 is installed above.
+sudo apt-get install -y --no-install-recommends libnvinfer6=6.0.1-1+cuda10.1 \
+    libnvinfer-dev=6.0.1-1+cuda10.1 \
+    libnvinfer-plugin6=6.0.1-1+cuda10.1
+
+# Following part adopted from https://github.com/NVIDIA/nvidia-docker#ubuntu-16041804-debian-jessiestretchbuster
+# Add the package repositories
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+sudo systemctl restart docker
+
+echo "Finished: please reboot"
---
+++ docs/run_local_setup.md
@@ -0,0 +1,28 @@
+# Running seed locally (tutorial for ubuntu):
+1. This tutorial assumes having:
+   + Ubuntu 18.04
+   + NVIDIA graphic card with compute capability
+	 matching  current tensorflow requirements
+		- you can check compute capability here:
+		 https://developer.nvidia.com/cuda-gpus
+		- you can check tensorflow requirements here:
+		https://www.tensorflow.org/install/gpu
+2. Install docker (see Prerequisites -> Docker)
+3. Install NVIDIA drivers
++ you can follow tutorial on tensorflow page (recommended):
+  https://www.tensorflow.org/install/gpu#ubuntu_1804_cuda_101
+  (please note that this is not the minimal installation)
++ or alternative for minimal installation (not recommended,
+  use only if above fails)
+```
+sudo add-apt-repository ppa:graphics-drivers
+
+sudo apt-get update
+
+sudo apt-get install nvidia-driver-440
+```
+3. Install nvidia-docker:
+   https://github.com/NVIDIA/nvidia-docker#quickstart
+   and reboot
+4. Now you can continue with: 
+   https://github.com/google-research/seed_rl#local-machine-training-on-a-single-level
