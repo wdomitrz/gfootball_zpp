c36974ad221fc153c448dcef8a6669114e919f68
CStanKonrad <CStanKonrad@users.noreply.github.com>
2020-04-13 20:40:32 +0200

    Reformat new loggers

--- gfootball_zpp/logging/cards.py
+++ gfootball_zpp/logging/cards.py
@@ -11,30 +11,34 @@ class LogCardsStatsTeam(LogBasicTracker):
 
     def _update_stats(self, observation):
         def convert_red_cards(team_active):
-            return map(lambda x: int(not x),team_active)
+            return map(lambda x: int(not x), team_active)
 
         self._first_team_yellow_cards = observation[0]['left_team_yellow_card']
-        self._first_team_red_cards = convert_red_cards(observation[0]['left_team_active'])
-        self._second_team_yellow_cards = observation[0]['right_team_yellow_card']
-        self._second_team_red_cards = convert_red_cards(observation[0]['right_team_active'])
-        
+        self._first_team_red_cards = convert_red_cards(
+            observation[0]['left_team_active'])
+        self._second_team_yellow_cards = observation[0][
+            'right_team_yellow_card']
+        self._second_team_red_cards = convert_red_cards(
+            observation[0]['right_team_active'])
 
-        print("yellow ", self._first_team_yellow_cards, " right:", self._second_team_yellow_cards)
-        print("yellow ", self._first_team_red_cards, " right:", self._second_team_red_cards)
-        
+        print("yellow ", self._first_team_yellow_cards, " right:",
+              self._second_team_yellow_cards)
+        print("yellow ", self._first_team_red_cards, " right:",
+              self._second_team_red_cards)
+
     def _write_logs(self, category):
         first_team_yellow = np.sum(self._first_team_yellow_cards)
         first_team_red = np.sum(self._first_team_red_cards)
         second_team_yellow = np.sum(self._second_team_yellow_cards)
         second_team_red = np.sum(self._second_team_red_cards)
-        self.summary_writer.write_scalar('{}/yellow_cards_first_team'.format(category),
-                                         first_team_yellow)
-        self.summary_writer.write_scalar('{}/red_cards_first_team'.format(category),
-                                         first_team_red)
-        self.summary_writer.write_scalar('{}/yellow_cards_second_team'.format(category),
-                                         second_team_yellow)
-        self.summary_writer.write_scalar('{}/red_cards_second_team'.format(category),
-                                         second_team_red)
+        self.summary_writer.write_scalar(
+            '{}/yellow_cards_first_team'.format(category), first_team_yellow)
+        self.summary_writer.write_scalar(
+            '{}/red_cards_first_team'.format(category), first_team_red)
+        self.summary_writer.write_scalar(
+            '{}/yellow_cards_second_team'.format(category), second_team_yellow)
+        self.summary_writer.write_scalar(
+            '{}/red_cards_second_team'.format(category), second_team_red)
 
     def __init__(self, env, config):
         LogBasicTracker.__init__(self, env, config)
@@ -47,7 +51,8 @@ class LogCardsStatsTeam(LogBasicTracker):
         env_episode_steps = self.env_episode_steps
         if env_episode_steps != 0:
             self._write_logs('cards')
-            self._write_logs('per_opponent_cards/' + get_opponent_name(self.env))
+            self._write_logs('per_opponent_cards/' +
+                             get_opponent_name(self.env))
 
         observation = super(LogCardsStatsTeam, self).reset()
 
--- gfootball_zpp/logging/game_mode.py
+++ gfootball_zpp/logging/game_mode.py
@@ -1,17 +1,11 @@
 from .utils import LogBasicTracker, EnvLogSteppingModes, get_opponent_name
 import tensorflow as tf
 
-
 NUM_GAME_MODES = 7
-GAME_MODE_DICT = dict([
-    (0, 'Normal'),
-    (1, 'KickOff'),
-    (2, 'GoalKick'),
-    (3, 'FreeKick'),
-    (4, 'Corner'),
-    (5, 'ThrowIn'),
-    (6, 'Penalty')
-])
+GAME_MODE_DICT = dict([(0, 'Normal'), (1, 'KickOff'), (2, 'GoalKick'),
+                       (3, 'FreeKick'), (4, 'Corner'), (5, 'ThrowIn'),
+                       (6, 'Penalty')])
+
 
 class LogGameModeStats(LogBasicTracker):
     def _trace_vars_reset(self):
@@ -23,14 +17,15 @@ class LogGameModeStats(LogBasicTracker):
         if self._prev_game_mode != current_game_mode:
             self._game_mode_counter[current_game_mode] += 1
             self._prev_game_mode = current_game_mode
-            print("gamemode changed to:", current_game_mode, " aka ", GAME_MODE_DICT[current_game_mode])
-        
+            print("gamemode changed to:", current_game_mode, " aka ",
+                  GAME_MODE_DICT[current_game_mode])
+
     def _write_logs(self, category):
         for gmid in range(NUM_GAME_MODES):
             game_mode_name = GAME_MODE_DICT[gmid]
             game_mode_count = self._game_mode_counter[gmid]
-            self.summary_writer.write_scalar('{}/{}'.format(category, game_mode_name),
-                                             game_mode_count)
+            self.summary_writer.write_scalar(
+                '{}/{}'.format(category, game_mode_name), game_mode_count)
 
     def __init__(self, env, config):
         LogBasicTracker.__init__(self, env, config)
@@ -43,7 +38,8 @@ class LogGameModeStats(LogBasicTracker):
         env_episode_steps = self.env_episode_steps
         if env_episode_steps != 0:
             self._write_logs('game_modes')
-            self._write_logs('per_opponent_game_modes/' + get_opponent_name(self.env))
+            self._write_logs('per_opponent_game_modes/' +
+                             get_opponent_name(self.env))
 
         observation = super(LogGameModeStats, self).reset()
 
