20821b91fa24623e9d2d602afb84c44c07735d7f
wdomitrz <witekdomitrz@gmail.com>
2020-04-19 14:26:27 +0200

    ?

---
+++ eval/eval.sh
@@ -0,0 +1,10 @@
+mkdir -p eval_logs
+mydir=$(pwd)
+
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR/../docker/build_eval.sh
+
+docker run -ti gfootball_zpp "$@"
+
