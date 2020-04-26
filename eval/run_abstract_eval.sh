d224e429d275fd3ab801ba3e072f7f465b21c5af
wdomitrz <witekdomitrz@gmail.com>
2020-04-26 19:34:51 +0200

    One more player

--- eval/run_abstract_eval.sh
+++ eval/run_abstract_eval.sh
@@ -1,4 +1,4 @@
-for name in academy_curriculum_eb opponents_curriculum_015 opponents_curriculum_0125 checkpoints_selfplay ; do
+for name in academy_curriculum_eb opponents_curriculum_015 opponents_curriculum_0125 checkpoints_selfplay randomized_env ; do
     echo $name
     ./run.sh --name $name
 done
