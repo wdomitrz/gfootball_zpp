9b4877c26eede6dbb98d00128ed93f0e88b23c2c
wdomitrz <witekdomitrz@gmail.com>
2020-04-26 15:39:29 +0200

    Eval fix

--- eval_docker_dir/run.sh
+++ eval_docker_dir/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-python gfootball_zpp/eval/main.py --logdir /eval_logs "$@"
+python /gfootball_zpp/gfootball_zpp/eval/main.py --logdir /eval_logs "$@"
 
 gsutil -m cp -r /eval_logs gs://marl-leaderboard
