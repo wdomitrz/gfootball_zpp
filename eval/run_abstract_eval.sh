2ae581d1acd9e267dc51ef49ec40c1cedc556f2f
wdomitrz <witekdomitrz@gmail.com>
2020-04-27 10:45:02 +0200

    One more oponent

--- eval/run_abstract_eval.sh
+++ eval/run_abstract_eval.sh
@@ -1,4 +1,4 @@
-for name in academy_curriculum_eb opponents_curriculum_015 opponents_curriculum_0125 checkpoints_selfplay randomized_env ; do
+for name in m_fsp_e71_64  academy_curriculum_eb opponents_curriculum_015 opponents_curriculum_0125 checkpoints_selfplay randomized_env ; do
     echo $name
     ./run.sh --name $name
 done
