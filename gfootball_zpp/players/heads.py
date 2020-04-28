d2bd7346b920ed53c3f989dea872fbb777767eb7
wdomitrz <witekdomitrz@gmail.com>
2020-04-28 15:17:48 +0200

    Number of players support

--- gfootball_zpp/players/heads.py
+++ gfootball_zpp/players/heads.py
@@ -7,11 +7,12 @@ from gfootball_zpp.utils import gsutil
 import numpy as np
 import tensorflow as tf
 
+CHANNELS_STEP = 16
 
 class HeadsPlayer(BaseZppPlayer):
     def __init__(self, controlled_players, player_config):
         sample = player_config.get('sample', False)
-        self.multihead = MultiHeadNet(DummyEnv('default', 4, controlled_agents=controlled_players), ())
+        self.multihead = MultiHeadNet(DummyEnv('default', controlled_players, controlled_agents=controlled_players), ())
         env = PackedBitsObservation(self.multihead)
         print('*' * 20)
         print(env.action_space.nvec)
@@ -20,8 +21,12 @@ class HeadsPlayer(BaseZppPlayer):
         self.net.change_config({'sample_actions': sample})
 
         # TODO: make it work with differen number of heads
-        sample_input = tf.convert_to_tensor(np.zeros((1, 1, 72, 96, 32)))
+        sample_input = tf.convert_to_tensor(np.zeros((1, 1, 72, 96, self._get_channels(controlled_players))))
         self.net(((), ((), (), sample_input)), ())
+
+    @staticmethod
+    def _get_channels(controlled_players):
+        return CHANNELS_STEP * (1 + (controlled_players + 2) // 4)
 
     def _expand_dims(self, obs):
         obs = packbits(obs)
