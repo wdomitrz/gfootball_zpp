3a26ffa49d35834e48dbd291252665e8eea1f924
CStanKonrad <CStanKonrad@users.noreply.github.com>
2020-05-03 18:16:00 +0200

    add nnm players

--- gfootball_zpp/eval/main.py
+++ gfootball_zpp/eval/main.py
@@ -1,6 +1,6 @@
 from absl import app, flags, logging
 from gfootball_zpp.eval.eval import evaluate_all, EvaluationStage, \
-    ZppEvalPlayerData, BotEvalPlayerData
+    ZppEvalPlayerData, BotEvalPlayerData, NNMEvalPlayerData
 from time import time
 import json
 
@@ -12,7 +12,7 @@ flags.DEFINE_string('logdir', '', 'Place to save results')
 
 FLAGS = flags.FLAGS
 
-GAMES = 5
+GAMES = 4
 
 ZPP_OPPONENTS = {
     'random': ZppEvalPlayerData('random', policy='random'),
@@ -31,11 +31,22 @@ ZPP_OPPONENTS = {
     'm_sp_bt_64_e2_p1': ZppEvalPlayerData('m_sp_bt_64_e2_p1', policy='multihead', sample='True', checkpoint='!latest-GS//m_sp/m_sp_bt_64_e2_p1/1/ckpt/0/'),
 }
 
+NNM_OPPONENTS = {
+    'm_sp_arch_longer_heads_bt_64_r100': NNMEvalPlayerData('m_sp_arch_longer_heads_bt_64_r100', models_dirs='GS//m_sp_arch/m_sp_arch_longer_heads_bt_64_r100/1/model/nn_manager/183;1.0;{᭤env_name᭤¦ ᭤5_vs_5_partially_randomized᭤᎖ ᭤enable_goal_videos᭤¦ false᎖ ᭤enable_full_episode_videos᭤¦ false᎖ ᭤logdir᭤¦ ᭤᭤᎖ ᭤enable_sides_swap᭤¦ false᎖ ᭤number_of_left_players_agent_controls᭤¦ 4᎖ ᭤number_of_right_players_agent_controls᭤¦ 0᎖ ᭤render᭤¦ false᎖ ᭤write_video᭤¦ false᎖ ᭤stacked_frames᭤¦ 4᎖ ᭤channel_dimensions᭤¦ [96᎖ 72]᎖ ᭤rewards᭤¦ ᭤scoring᎖checkpoints᭤᎖ ᭤dump_frequency᭤¦ 10᎖ ᭤wrappers᭤¦ ᭤checkpoint_score᎖obs_extract᎖single_agent᎖obs_stack᎖old_single_map᎖pack_bits᭤}'),
+    
+    'm_sp_arch_separate_baseline_r101': NNMEvalPlayerData('m_sp_arch_separate_baseline_r101', models_dirs='GS//m_sp_arch/m_sp_arch_separate_baseline_r101/1/model/nn_manager/181;1.0;{᭤env_name᭤¦ ᭤5_vs_5_partially_randomized᭤᎖ ᭤enable_goal_videos᭤¦ false᎖ ᭤enable_full_episode_videos᭤¦ false᎖ ᭤logdir᭤¦ ᭤᭤᎖ ᭤enable_sides_swap᭤¦ false᎖ ᭤number_of_left_players_agent_controls᭤¦ 4᎖ ᭤number_of_right_players_agent_controls᭤¦ 0᎖ ᭤render᭤¦ false᎖ ᭤write_video᭤¦ false᎖ ᭤stacked_frames᭤¦ 4᎖ ᭤channel_dimensions᭤¦ [96᎖ 72]᎖ ᭤rewards᭤¦ ᭤scoring᎖checkpoints᭤᎖ ᭤dump_frequency᭤¦ 10᎖ ᭤wrappers᭤¦ ᭤checkpoint_score᎖obs_extract᎖single_agent᎖obs_stack᎖old_single_map᎖pack_bits᭤}'),
+    
+    'm_sp_arch_smiple115_r100': NNMEvalPlayerData('m_sp_arch_smiple115_r100', models_dirs='GS//m_sp_arch/m_sp_arch_smiple115_r100/1/model/nn_manager/183;1.0;{᭤env_name᭤¦ ᭤5_vs_5_partially_randomized᭤᎖ ᭤enable_goal_videos᭤¦ false᎖ ᭤enable_full_episode_videos᭤¦ false᎖ ᭤logdir᭤¦ ᭤᭤᎖ ᭤enable_sides_swap᭤¦ false᎖ ᭤number_of_left_players_agent_controls᭤¦ 4᎖ ᭤number_of_right_players_agent_controls᭤¦ 0᎖ ᭤render᭤¦ false᎖ ᭤write_video᭤¦ false᎖ ᭤stacked_frames᭤¦ 4᎖ ᭤channel_dimensions᭤¦ [96᎖ 72]᎖ ᭤rewards᭤¦ ᭤scoring᎖checkpoints᭤᎖ ᭤dump_frequency᭤¦ 10᎖ ᭤wrappers᭤¦ ᭤checkpoint_score᎖simple115v2᎖simple115_pick_first᭤}'),
+    
+    
+}
+
+
 ABSTRACT_OPPONENTS = {
     'random': ZppEvalPlayerData('random', policy='random'),
-    'random_net': ZppEvalPlayerData('random_net', policy='multihead', sample='True'),
+    #'random_net': ZppEvalPlayerData('random_net', policy='multihead', sample='True'),
     'academy_curriculum_eb': ZppEvalPlayerData('academy_curriculum_eb', policy='multihead', sample='True', checkpoint='GS//academy_5vs5_transfer/academy_5vs5_transfer_eval_3/1/ckpt/0/ckpt-809'),
-    'opponents_curriculum_015': ZppEvalPlayerData('opponents_curriculum_015', policy='multihead', sample='True', checkpoint='GS//f5v01/f5v0to1to5_e2/1/ckpt/0/ckpt-252'),
+    #'opponents_curriculum_015': ZppEvalPlayerData('opponents_curriculum_015', policy='multihead', sample='True', checkpoint='GS//f5v01/f5v0to1to5_e2/1/ckpt/0/ckpt-252'),
     'opponents_curriculum_0125': ZppEvalPlayerData('opponents_curriculum_0125', policy='multihead', sample='True', checkpoint='GS//f5v01/f5v0to1to2t05_e2/1/ckpt/0/ckpt-291'),
     'checkpoints_selfplay': ZppEvalPlayerData('checkpoints_selfplay_e9_sp', sample=True, policy='multihead', checkpoint='GS//zuzanna-seed/checkpoints_sp/checkpoints_selfplay_e9/1/ckpt/0/ckpt-274'),
     'm_sp_in_bt_4_e2_p1': ZppEvalPlayerData('m_sp_in_bt_4_e2_p1', policy='multihead', sample='True', checkpoint='GS//m_sp/m_sp_in_bt_4_e2_p1/1/ckpt/0/ckpt-294')
@@ -68,6 +79,10 @@ ABSTRACT_BOT_STAGES = [
     EvaluationStage('academy_5_vs_5_corner', BotEvalPlayerData('academy_5_vs_5_corner', 0.05), GAMES)
 ]
 
+AGENT_OPPONENTS = {**ZPP_OPPONENTS, **NNM_OPPONENTS}
+
+OPPONENTS = {**NNM_OPPONENTS, **ABSTRACT_OPPONENTS}
+
 ENV_ARGS = {
     'write_goal_dumps': False,
     'write_full_episode_dumps': True,
@@ -85,12 +100,12 @@ def build_stages(filter=None):
     if not filter:
         filter = []
     stages = BOTS_STAGES + []
-    for opponent in ZPP_OPPONENTS:
+    for opponent in OPPONENTS:
         if opponent in filter:
             continue
         for scenario in ZPP_SCENARIOS:
             stages.append(EvaluationStage(
-                scenario, ZPP_OPPONENTS[opponent], GAMES))
+                scenario, OPPONENTS[opponent], GAMES))
     return stages
 
 
@@ -107,9 +122,9 @@ def build_summary(player, results):
 
 def main(args):
     logging.info("Starting evaluation of %s.", FLAGS.name)
-    assert FLAGS.name in ZPP_OPPONENTS, "There is no such player definition!"
+    assert FLAGS.name in AGENT_OPPONENTS, "There is no such player definition!"
 
-    player = ZPP_OPPONENTS[FLAGS.name]
+    player = AGENT_OPPONENTS[FLAGS.name]
     opponents_to_filer = FLAGS.filter_opponents
     # I guess we want also selfplay results
     # opponents_to_filer.append(FLAGS.name)
