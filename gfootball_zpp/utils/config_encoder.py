cd3b231a58e515b58583061b153a962907d7e360
wdomitrz <witekdomitrz@gmail.com>
2020-05-13 01:17:45 +0200

    To review

--- gfootball_zpp/utils/config_encoder.py
+++ gfootball_zpp/utils/config_encoder.py
@@ -32,12 +32,43 @@ def decode_config(str_config):
     return cfg
 
 
+def maybe_str(s):
+    if isinstance(s, str):
+        return s
+    return str(s)
+
+def encode_models_dirs(model_dirs_cfg):
+    """
+    The config for model_dirs accepts a list of dicts. Each dict should contain
+    "path", "proba" and "config" keys. The "config" key will be parsed by the
+    `encode_config` function.
+    """
+    each_model_parsed = [';'.join([maybe_str(model_dir_cfg["path"]),
+                                   maybe_str(model_dir_cfg["proba"]),
+                                   encode_config(model_dir_cfg["config"])])
+                         for model_dir_cfg in model_dirs_cfg]
+    return '*'.join(each_model_parsed)
+
+
+def encode_config_extra_player(player_name, player_cfg):
+    if "models_dirs" in player_cfg:
+        player_cfg["model_dirs"] = encode_models_dirs(player_cfg["models_dirs"])
+    return player_name + ':' + ','.join(maybe_str(name) + '=' + maybe_str(value) for name, value in player_cfg.items())
+
+
+def encode_config_base(cfg):
+    if "extra_players" in cfg:
+        cfg["extra_players"] = [encode_config_extra_player(
+            *player_name_and_cfg) for player_name_and_cfg in cfg["extra_players"].items()]
+    return cfg
+
+
 def main(argv):
     with open(FLAGS.f) as f:
         str_config = f.read()
         cfg = json.loads(str_config)
-        print(encode_config(cfg))
+        print(encode_config_base(cfg))
 
 
 if __name__ == '__main__':
-  app.run(main)
+    app.run(main)
