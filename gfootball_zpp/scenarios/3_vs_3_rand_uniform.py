66302dae30fd865c653e68cca339f3720c064213
CStanKonrad <CStanKonrad@users.noreply.github.com>
2020-05-15 16:40:18 +0200

    fix scenario

--- gfootball_zpp/scenarios/3_vs_3_rand_uniform.py
+++ gfootball_zpp/scenarios/3_vs_3_rand_uniform.py
@@ -14,11 +14,15 @@ def build_scenario(builder):
         first_team = Team.e_Right
         second_team = Team.e_Left
 
+    
+    #ball_x, ball_y, _ = get_player_position_from_uniform(-0.4, -0.2, 0.4, 0.2, e_PlayerRole_CF)
+    #builder.SetBallPosition(ball_x, ball_y)
+
     builder.SetTeam(first_team)
-    builder.AddPlayer(-1.000000, 0.000000, e_PlayerRole_GK, controllable=False)
+    builder.AddPlayer(-100.000000, 0.000000, e_PlayerRole_GK, controllable=False)
     builder.AddPlayer(*get_player_position_from_uniform(-0.8, -0.45, 0.8, 0.45, e_PlayerRole_CF))
     builder.AddPlayer(*get_player_position_from_uniform(-0.8, -0.45, 0.8, 0.45, e_PlayerRole_CF))
     builder.SetTeam(second_team)
-    builder.AddPlayer(-1.000000, 0.000000, e_PlayerRole_GK, controllable=False)
+    builder.AddPlayer(-100.000000, 0.000000, e_PlayerRole_GK, controllable=False)
     builder.AddPlayer(*get_player_position_from_uniform(-0.8, -0.45, 0.8, 0.45, e_PlayerRole_CF))
     builder.AddPlayer(*get_player_position_from_uniform(-0.8, -0.45, 0.8, 0.45, e_PlayerRole_CF))
